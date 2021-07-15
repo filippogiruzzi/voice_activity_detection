7cd57deb45bc5cb96ef2bf0a3db41bd14886b431
Filippo Giruzzi <filippo.giruzzi@hotmail.fr>
2021-07-15 15:35:05 +0200

    minor clean ups for CI & linters

--- scripts/docker_local.sh
+++ scripts/docker_local.sh
@@ -2,10 +2,7 @@
 
 docker run --rm \
 		--gpus all \
-		--user root \
 		-v /var/run/docker.sock:/var/run/docker.sock \
-		-v /home/filippo/Datasets/perso:/data \
-		-v /root:/root \
 		-it \
 		--entrypoint /bin/bash \
 		-e TF_FORCE_GPU_ALLOW_GROWTH=true \
