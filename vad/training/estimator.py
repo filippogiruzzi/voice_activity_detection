3437e12f326fdd698d742c4197fe3797c4521e93
Filippo Giruzzi <filippo.giruzzi@hotmail.fr>
2021-09-23 16:43:33 +0200

    :rotating_light: pep257 refacto for estimator

--- vad/training/estimator.py
+++ vad/training/estimator.py
@@ -1,23 +1,61 @@
+"""Estimator class for Deep Learning VAD models."""
 import tensorflow as tf
 
 from vad.training.model import Resnet1D
 
 
 class VadEstimator(object):
+    """VAD Estimator class.
+
+    Args:
+        object ([type]): [description]
+    """
+
     def __init__(self, params):
+        """Initialize VadEstimator class parameters.
+
+        Args:
+            params (dict): dictionary of model parameters
+        """
         self._instantiate_model(params)
 
     def _instantiate_model(self, params, training=False):
+        """Utilitary function to initalize model parameters.
+
+        Args:
+            params (dict): dictionary of model parameters
+            training (bool, optional): training or inference. Defaults to False.
+        """
         if params["model"] == "resnet1d":
             self.model = Resnet1D(params=params, is_training=training)
 
     def _output_network(self, features, params, training=False):
+        """Run model prediction.
+
+        Args:
+            features (tf.Tensor): input MFCC features
+            params (dict): dictionary of model parameters
+            training (bool, optional): training or inference. Defaults to False.
+
+        Returns:
+            output (tf.Tensor): output predictions
+        """
         self._instantiate_model(params=params, training=training)
         output = self.model(inputs=features)
         return output
 
     @staticmethod
     def loss_fn(labels, predictions, params):
+        """Loss function for training.
+
+        Args:
+            labels (dict): dictionary of labels
+            predictions (dict): dictionary of predictions
+            params (dict): dictionary of model parameters
+
+        Returns:
+            loss (tf.Tensor): loss value
+        """
         pred = predictions["speech"]
         label = labels["label"]
 
@@ -27,6 +65,17 @@ class VadEstimator(object):
         return loss
 
     def model_fn(self, features, labels, mode, params):
+        """Model wrapper function.
+
+        Args:
+            features (tf.Tensor): input MFCC features
+            labels (dict): dictionary of labels
+            mode (str): model mode, one of ["train", "eval", "infer"]
+            params (dict): dictionary of model parameters
+
+        Returns:
+            (tf.estimator.EstimatorSpec): ops returned from the model function
+        """
         training = mode == tf.estimator.ModeKeys.TRAIN
         preds = self._output_network(features, params, training=training)
 
@@ -51,13 +100,16 @@ class VadEstimator(object):
             pred_val = tf.round(tf.nn.sigmoid(predictions["speech"]))
             metrics = {
                 "accuracy/accuracy/acc": tf.metrics.accuracy(
-                    labels=labels["label"], predictions=pred_val
+                    labels=labels["label"],
+                    predictions=pred_val,
                 )
             }
 
             loss = self.loss_fn(labels, predictions, params)
             return tf.estimator.EstimatorSpec(
-                mode=mode, loss=loss, eval_metric_ops=metrics
+                mode=mode,
+                loss=loss,
+                eval_metric_ops=metrics,
             )
 
         # Prediction op
@@ -71,5 +123,7 @@ class VadEstimator(object):
                 "predictions": tf.estimator.export.PredictOutput(predictions)
             }
             return tf.estimator.EstimatorSpec(
-                mode=mode, predictions=predictions, export_outputs=export_outputs
+                mode=mode,
+                predictions=predictions,
+                export_outputs=export_outputs,
             )
